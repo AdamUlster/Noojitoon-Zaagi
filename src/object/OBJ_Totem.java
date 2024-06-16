afe291b263a6e39c77aef6f3fc8bb9030fc0045f
Russe <russell.lau-uy@student.tdsb.on.ca>
2024-06-16 00:40:28 -0400

    OBJECT COMMENTS

--- src/object/OBJ_Totem.java
+++ src/object/OBJ_Totem.java
@@ -3,21 +3,27 @@ package object;
 import entity.Entity;
 import main.GamePanel;
 
+// TOTEMS THAT ARE COLLECTED BY THE PLAYER
 public class OBJ_Totem extends Entity {
-    public double hitboxScale = 0.80;//scaling factor for hitbox
-    public OBJ_Totem(GamePanel gp) { // constructor
-        super(gp); // calls the entity class
+//    SCALING FACTOR FOR HIT BOX
+    public double hitboxScale = 0.80;
+
+//    CONSTRUCTOR
+    public OBJ_Totem(GamePanel gp) {
+        super(gp); // CALL ENTITY CLASS
 
         name = "Totem";
+//        SINCE DEFAULT DIRECTION IS DOWN
         down1 = setup("objects/totem", 1,1);
-        collision = true;
+        collision = true;// MAKE TOTEM AN COLLIDEABLE OBKECT
 
-        //create hitbox in the center
+//        SET HIT BOX TO THE CENTER
         solidArea.width = (int)(gp.tileSize * hitboxScale);
         solidArea.height = (int)(gp.tileSize * hitboxScale);
         solidArea.x = (gp.tileSize - solidArea.width) / 2;
         solidArea.y = (gp.tileSize - solidArea.height) / 2;
 
+//        SET UP COLLISION BOX COORDINATES
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
     }
